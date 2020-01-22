593d2220ca465a2be9f7da1bcfd2b368a1415ce4
strtomek <str.tomek@gmail.com>
2020-01-22 18:19:42 +0100

    configured mongodb connection

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose')
 
 const dotenv = require('dotenv');
 dotenv.config();
 
+// mongoose.connect('mongodb+srv://' + process.env.USERNAME + ':' + process.env.PASSWORD + '@interpretowane-7ahy1.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+//     useUnifiedTopology: true, useNewUrlParser: true,
+
+// })
+mongoose.connect('mongodb+srv://inter4:' + process.env.PASSWORD +'@interpretowane-7ahy1.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+    useUnifiedTopology: true, useNewUrlParser: true,
+
+})
+    .then(e => {
+        console.log(e);
+    })
+    .catch(e => {
+        console.log(e);
+    })
 app.use(morgan('dev'));
 
-// app.get('/', (req, res, next) => {
-//     res.send('hi');
-// });
+app.get('/', (req, res, next) => {
+    res.send('hi');
+});
 
 var server = app.listen(process.env.PORT);
 
